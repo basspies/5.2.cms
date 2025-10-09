ddab3e0151109e790181bb429ca91f5265082b20
basspies <haroldspies@hotmail.com>
2025-10-09 15:56:05 +0200

    commit

--- includes/header.php
+++ includes/header.php
@@ -7,3 +7,18 @@
     <title>My CMS</title>
 </head>
 <body>
+
+<header>
+    <div class="header-container">
+
+        <img src="images/logo.png" alt="Site Logo" class="logo">
+
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+
+    </div>
