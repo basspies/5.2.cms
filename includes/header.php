f22b5f413f6afed3a44b43fe7a62060f6ea9110d
basspies <haroldspies@hotmail.com>
2025-10-10 12:09:07 +0200

    header

--- includes/header.php
+++ includes/header.php
@@ -11,10 +11,10 @@
 <header>
     <div class="header-container">
 
-        <img src="images/logo.png" alt="Site Logo" class="logo">
+        <img src="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBwgHBgkIBwgKCgkLDRYPDQwMDRsUFRAWIB0iIiAdHx8kKDQsJCYxJx8fLT0tMTU3Ojo6Iys/RD84QzQ5OjcBCgoKDQwNGg8PGjclHyU3Nzc3Nzc3MTc3Nzc3Nzc3Nzc3NzA0LS03Nzc3LTctNzc1KzU1NTc1NzUrLS04Ky03Nf/AABEIABwAHAMBEQACEQEDEQH/xAAaAAABBQEAAAAAAAAAAAAAAAAAAgMEBgcF/8QAKBAAAgICAQMDAwUAAAAAAAAAAQIDBAARBRIhMRNRYQZBkRQVIiNx/8QAGAEBAAMBAAAAAAAAAAAAAAAAAAECAwX/xAAiEQACAgEDBAMAAAAAAAAAAAAAAgERAxMhMQQSUWEiMkH/2gAMAwEAAhEDEQA/ANpoWluVxKo0QzIw9mUlSPyDmeN4dbBIzQBgBgFQ+oUlrVuKr8RLLF0tKyqrkFyiF9N77IO9/c5y+piVVFxEEH9xs3LnIXksTpXn42xJBH1kBQmlDAfY7BO8xnK7sz3tKzQGeVtWV/mHusE42uweK0yLE7bHWw33763kZ2a73+sfvHsDv1S/MRXa6wcvFAP0ydS+v0dTd9nXzluqnNDR2vW0Elrr+ncaSxCsbBJf6JSN99AMVPse4zqqsS0zMGcN3Raihx6qI1Fet0hPSIEYGoz5UfHxltPF4HzFGgvpuohrkugjbcY0UA7L/mTp4/BNPWwzNxEMzhpKlKTShQXhB0APHjxkaWGeVKyuS+TpqqooVFCqBoAeBljSq2gVgkMAMA//2Q==" alt="Site Logo" class="logo">
 
         <nav>
-            <ul>
+            <ul class="header-links">
                 <li><a href="index.php">Home</a></li>
                 <li><a href="about.php">About</a></li>
                 <li><a href="contact.php">Contact</a></li>
@@ -22,3 +22,4 @@
         </nav>
 
     </div>
+    
--- index.php
+++ index.php
@@ -1,17 +1,48 @@
 <?php include 'includes/header.php';?>
 
+<div class="slideshow-images">
+    <div class="mySlides-fade">
+        <img src="images/slide1.jpg" style="width:100%">
+    </div>
 
+    <div class="mySlides-fade">
+        <img src="images/slide2.jpg" style="width:100%">
+    </div>
 
+    <div class="mySlides-fade">
+        <img src="images/slide3.jpg" style="width:100%">
+    </div>
 
+</div>
 
+<div class="slideshow-buttons">
+    <button class="prev" onclick="plusSlides(-1)">&#10094;</button>
+    <button class="next" onclick="plusSlides(1)">&#10095;</button>
+</div>
 
+<script>
+    let slideIndex = 1;
+    showSlides(slideIndex);
 
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
 
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
 
-
-
-
-
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex-1].style.display = "block";
+    }
+</script>
 
 
 <?php include 'includes/footer.php';?>
