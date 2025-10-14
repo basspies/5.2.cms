5fcb13a6c3cd5c5716205567c334de73aa3b2b31
basspies <haroldspies@hotmail.com>
2025-10-14 12:00:43 +0200

    committ slideshow

--- index.php
+++ index.php
@@ -1,48 +1,34 @@
 <?php include 'includes/header.php';?>
 
-<div class="slideshow-images">
-    <div class="mySlides-fade">
-        <img src="images/slide1.jpg" style="width:100%">
+<div class="slideshow-container">
+
+    <div class="slide fade">
+      <img src="https://picsum.photos/id/1015/800/400" alt="Afbeelding 1">
+      <div class="caption">Berglandschap</div>
     </div>
 
-    <div class="mySlides-fade">
-        <img src="images/slide2.jpg" style="width:100%">
+    <div class="slide fade">
+      <img src="https://picsum.photos/id/1016/800/400" alt="Afbeelding 2">
+      <div class="caption">Zonsondergang</div>
     </div>
 
-    <div class="mySlides-fade">
-        <img src="images/slide3.jpg" style="width:100%">
+    <div class="slide fade">
+      <img src="https://picsum.photos/id/1020/800/400" alt="Afbeelding 3">
+      <div class="caption">Meer met bergen</div>
     </div>
 
-</div>
+    <!-- Navigatie pijlen -->
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+  </div>
 
-<div class="slideshow-buttons">
-    <button class="prev" onclick="plusSlides(-1)">&#10094;</button>
-    <button class="next" onclick="plusSlides(1)">&#10095;</button>
-</div>
+  <div class="dots">
+    <span class="dot" onclick="currentSlide(1)"></span>
+    <span class="dot" onclick="currentSlide(2)"></span>
+    <span class="dot" onclick="currentSlide(3)"></span>
+  </div>
 
-<script>
-    let slideIndex = 1;
-    showSlides(slideIndex);
-
-    function plusSlides(n) {
-        showSlides(slideIndex += n);
-    }
-
-    function currentSlide(n) {
-        showSlides(slideIndex = n);
-    }
-
-    function showSlides(n) {
-        let i;
-        let slides = document.getElementsByClassName("mySlides");
-        if (n > slides.length) {slideIndex = 1}
-        if (n < 1) {slideIndex = slides.length}
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        slides[slideIndex-1].style.display = "block";
-    }
-</script>
+  <script src="script.js"></script>
 
 
 <?php include 'includes/footer.php';?>
