fb25bc5b1516cdff620f175e0b397329751505f3
basspies <haroldspies@hotmail.com>
2025-10-28 12:40:46 +0100

    commit crud systeem

--- cms_paginas/header_cms.php
+++ cms_paginas/header_cms.php
@@ -8,7 +8,7 @@ if (session_status() === PHP_SESSION_NONE) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <title>praktijk mirre</title>
 </head>
 <body>
@@ -16,7 +16,7 @@ if (session_status() === PHP_SESSION_NONE) {
 <header>
     <div class="header-container">
 
-        <img src="img/mirre_logo.png" alt="Site Logo" class="logo">
+        <img src="../img/mirre_logo.png" alt="Site Logo" class="logo">
 
         <nav>
             <ul class="header-links">
--- cms_paginas/over_mij_cms.php
+++ cms_paginas/over_mij_cms.php
@@ -1,6 +1,6 @@
 <?php 
-include 'includes/header.php';
-include 'includes/connect.php';
+include 'header_cms.php';
+include '../includes/connect.php';
 ?>
 
 <div class="cms-intro">
@@ -10,15 +10,18 @@ include 'includes/connect.php';
 <div class="cms-container">
 
     <ul class="cms-links">
-        <li><a href="cms_paginas/homepagina_cms.php">homepage</a></li>
-        <li><a href="cms_paginas/over_mij_cms.php">over mij</a></li>
-        <li><a href="cms_paginas/contact_gegevens_cms.php">contact_gegevens</a></li>
-        <li><a href="index.php">Uitloggen</a></li>
+        <li><a href="homepagina_cms.php">homepage</a></li>
+        <li><a href="over_mij_cms.php">over mij</a></li>
+        <li><a href="contact_gegevens_cms.php">contact_gegevens</a></li>
+        <li><a href="../index.php">Uitloggen</a></li>
     </ul>
 
     <div class="cms-inhoud">
         <h3>Inhoud bewerken</h3>
         <p>Kies een sectie om te bewerken vanuit het menu hierboven.</p>
+        <div class="slideshow_cms">
+            
+        </div>
     </div>
 </div>
 
