6021a4470cf3997b274a8c10ecbfbe73d780e810
basspies <haroldspies@hotmail.com>
2025-10-30 00:02:25 +0100

    commit tarieven pagina

--- over_mij.php
+++
@@ -1,8 +0,0 @@
-<?php include 'includes/header.php'; ?>
-
-<div class="container">
-    <h1>Over Mij</h1>
-    <p>Welkom op de 'Over Mij' pagina!</p>
-</div>
-
-<?php include 'includes/footer.php'; ?>
---
+++ tarieven.php
@@ -0,0 +1,21 @@
+<?php include 'includes/header.php'; ?>
+
+<div class="container">
+    <h1>tarieven</h1>
+</div>
+
+<div class="tarieven_container">
+    <div class="tarieven_soorten">
+        <p>Kennismakingsgesprek:  gratis (30 minuten)</p>
+    </div>
+
+    <div class="tarieven_soorten">
+        <p>Individuele begeleiding/behandeling: € 70,00 per sessie. (60 minuten)</p>
+    </div>
+
+    <div class="tarieven_soorten">
+        <p>Wandel coaching: € 70,00 per sessie. (60 minuten)</p>
+    </div>
+</div>
+
+<?php include 'includes/footer.php'; ?>
